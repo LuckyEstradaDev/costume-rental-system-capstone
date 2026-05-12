68b2b776af19da5ec13084ded72264ac22cf1d52
Aragon <aragoncarlmatel2@gmail.com>
2026-05-12 20:58:34 +0800

    deduct stocks feature need to be fixed ;)

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -2,7 +2,7 @@
 import {v2 as cloudinary} from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dxuh7jujl",
+  cloud_name: "dis4gnyua",
   api_key: process.env.CLOUDINARY_API_KEY || "",
   api_secret: process.env.CLOUDINARY_SECRET_KEY || "",
 });
--- backend/src/repositories/OrderRepository.ts
+++ backend/src/repositories/OrderRepository.ts
@@ -1,9 +1,22 @@
 import type {IOrder} from "../interfaces/IOrder.js";
+import type { IOutfit } from "../interfaces/IOutfit.js";
+import type { Snapshot } from "../interfaces/ISnapshot.js";
 import {OrderModel} from "../models/OrderModel.js";
+import { OutfitModel } from "../models/OutfitModel.js";
 
 export class OrderRepository {
   async create(orderData: IOrder) {
-    return await OrderModel.create(orderData);
+    const order = OrderModel.create(orderData);
+    const outfit: any = orderData.items.map((item: Snapshot) => {
+      return {outfitID: item.outfitId, variantID: item.variantId, quantity: item.quantity};
+    });
+
+    //deduct stocks from the outfit variants when placing orders
+    outfit.forEach((item: any) => {
+      OutfitModel.findByIdAndUpdate(item.outfitID, {$inc: {[`variants.$[variant].stock`]: -item.quantity}}, {arrayFilters: [{'variant._id': item.variantID}]}).exec(); 
+    });
+
+    return await order
   }
 
   async getByUserId(userId: string) {
