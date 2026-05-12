53bcdb14459cd47dcf6109313774a407678c6790
Lucky Estrada <estrada.lnp@gmail.com>
2026-05-13 01:37:04 +0800

    deduct stocks when rent has been made

--- backend/src/repositories/RentRepository.ts
+++ backend/src/repositories/RentRepository.ts
@@ -1,9 +1,36 @@
 import type {INewRent} from "../interfaces/IRent.js";
+import type {Snapshot} from "../interfaces/ISnapshot.js";
+import {OutfitModel} from "../models/OutfitModel.js";
 import {RentModel} from "../models/RentModel.js";
 
 export class RentRepository {
   async createRent(data: INewRent) {
-    const rent = await RentModel.create(data);
+    const rent = RentModel.create(data);
+    const outfit: any = data.items.map((item: Snapshot) => {
+      return {
+        outfitID: item.outfitId,
+        variantID: item.variantId,
+        size: item.size,
+        quantity: item.quantity,
+      };
+    });
+
+    //deduct stocks from the outfit variants when placing orders
+    await Promise.all(
+      outfit.map((item: any) =>
+        OutfitModel.findByIdAndUpdate(
+          item.outfitID,
+          {$inc: {[`variants.$[variant].sizes.$[size].stock`]: -item.quantity}},
+          {
+            arrayFilters: [
+              {"variant._id": item.variantID},
+              {"size.size": item.size},
+            ],
+          },
+        ).exec(),
+      ),
+    );
+
     return rent;
   }
 
