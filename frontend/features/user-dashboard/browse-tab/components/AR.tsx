b4e1957997f40012027a8f6e2308e2f4ae3c4f2b
Matelharu <aragoncarlmatel2@gmail.com>
2026-06-14 12:00:45 +0800

    Adjust width and height calculations in AR component

--- frontend/features/user-dashboard/browse-tab/components/AR.tsx
+++ frontend/features/user-dashboard/browse-tab/components/AR.tsx
@@ -99,7 +99,7 @@ export function AR({
                   Math.hypot(rightH.y - rightS.y, rightH.x - rightS.x)) /
                 2;
 
-              const width = shoulderWidth * 3;
+              const width = shoulderWidth * 2.5;
 const height = torsoHeight * 2.8;
               let angle = Math.atan2(rightS.y - leftS.y, rightS.x - leftS.x);
               if (angle > Math.PI / 2 || angle < -Math.PI / 2) {
@@ -112,7 +112,7 @@ const height = torsoHeight * 2.8;
               ctx.drawImage(
                 shirtImg,
                 -width / 2,
-                -height * 0.35,
+                -height * 0.30,
                 width,
                 height,
               );
