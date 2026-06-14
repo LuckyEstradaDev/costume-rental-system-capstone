d89436f4bd86f9cabdc85af4042e2e329fdc480b
Matelharu <aragoncarlmatel2@gmail.com>
2026-06-14 11:50:51 +0800

    Update AR.tsx

--- frontend/features/user-dashboard/browse-tab/components/AR.tsx
+++ frontend/features/user-dashboard/browse-tab/components/AR.tsx
@@ -112,7 +112,7 @@ const height = torsoHeight * 2.8;
               ctx.drawImage(
                 shirtImg,
                 -width / 2,
-                -height * 0.35,
+                -height * 0.50,
                 width,
                 height,
               );
